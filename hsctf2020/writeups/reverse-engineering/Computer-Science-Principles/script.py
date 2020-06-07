8b80a54c2795bb9a5cbae97f56203e7687bb3468
GreenAvocado <greenavocado@protonmail.com>
2020-06-07 14:38:50 -0600

    feat: cleaned up script

--- hsctf2020/writeups/reverse-engineering/Computer-Science-Principles/script.py
+++ hsctf2020/writeups/reverse-engineering/Computer-Science-Principles/script.py
@@ -3,8 +3,14 @@
 def unshift(encrypted):
     ret = ""
     for i in range(len(encrypted)):
-        ret += chr(ord(encrypted[i]) + i - len(str(ord(encrypted[i]))))
+        ret += chr(ord(encrypted[i]) + i)
     return ret
 
-print(unshift("inagzgkpm)Wl&Tg&io"))
+def unshift2(encrypted):
+    ret = ""
+    for i in range(len(encrypted)):
+        ret += chr(ord(encrypted[i]) - len(str(ord(encrypted[i]) - 2)))
+    return ret
+
+print(unshift(unshift2("inagzgkpm)Wl&Tg&io")))
 
