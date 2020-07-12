aff5d9d354e25dfe3b2eb254d82ac31ae8e0aee1
GreenAvocado <greenavocado@protonmail.com>
2020-07-12 12:23:20 -0600

    feat: solved Too-Slow

--- rgbctf2020/challenges/pwn-rev/Too-Slow/solve.c
+++ rgbctf2020/challenges/pwn-rev/Too-Slow/solve.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main()
+void win(unsigned long param_1)
 {
     unsigned long flag = 0x12297e12426e6f53;
+    unsigned long junk0 = 0x79242e48796e7141;
+    unsigned long junk1 = 0x49334216426e2e4d;
+    unsigned long junk2 = 0x473e425717696a7c;
+    unsigned long junk3 = 0x42642a41;
+    unsigned long junk4 = 0;
     unsigned int counter = 0;
-    unsigned long param_1 = 0x265d1d23;
 
     while (counter < 9) {
         *(unsigned int *)((long)&flag + (unsigned long)counter * 4) =
@@ -14,6 +18,23 @@ int main()
 
     printf("Your flag: rgbCTF{%36s}\n",&flag);
 
+    return;
+}
+
+unsigned long getKey()
+{
+    unsigned int a;
+
+    a = 0;
+    while (a < 0x265d1d23) {
+        a = a + 1;
+    }
+    return (unsigned long)a;
+}
+
+int main()
+{
+    win(getKey());
     return 0;
 }
 
